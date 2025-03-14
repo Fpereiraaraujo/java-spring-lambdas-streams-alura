74e56a1614fb8c5da81a0a0787c88c541c602c28
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2025-03-14 17:25:09 -0300

    feat:modelando dados das series

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.services.ConsumoApi;
+import br.com.alura.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +18,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
+
+		ConverteDados converteDados = new ConverteDados();
+		DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
+
+		System.out.println(dadosSerie);
 	}
 
 }
