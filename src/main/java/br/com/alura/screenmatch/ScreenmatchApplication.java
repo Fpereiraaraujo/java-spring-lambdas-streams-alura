740400cb3a02da97c5792dd67ae418b88b838c35
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2025-03-14 17:09:08 -0300

    feat:criando responsabilidade separada de consumir api

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,22 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.services.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
 	}
 
 }
